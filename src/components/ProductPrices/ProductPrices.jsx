3cbf00c62312b3cf50f37e8bc52fe40ccf8e5481
Nazar Melnik <nazar97melnik@gmail.com>
2023-10-23 11:58:51 +0300

    do object which have key like id and value like array from the prices also do all with the state and without glogal const,this app will be fine also with 100k products

--- src/components/ProductPrices/ProductPrices.jsx
+++ src/components/ProductPrices/ProductPrices.jsx
@@ -1,35 +1,40 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function ProductPrices(props) {
   const { product, id } = props;
-
-  // Створюємо стан для зберігання видимості для кожного ID
-  const [visibleIDs, setVisibleIDs] = useState({});
-
-  // Функція для зміни видимості для певного ID
-  const toggleVisibility = (id) => {
-    setVisibleIDs((prevVisibleIDs) => ({
-      [id]: !prevVisibleIDs[id],
-    }));
-  };
-
-  // Генеруємо `li` елементи для всіх можливих ID
-  const listItems = product.map((product) => (
-    <li
-      key={product.id}
-      style={{ display: visibleIDs[product.id] ? "block" : "none" }}>
-      <p>
-        ID {product.id} - {product.price}
-      </p>
-    </li>
-  ));
-
-  return (
-    <>
-      <ul>{listItems}</ul>
-      <button onClick={() => toggleVisibility(id)}>
-        Показати товари з ID {id}
-      </button>
-    </>
-  );
+  const [prices, setPrices] = useState({});
+  // проходимось по кожному елементу після отримання данних з запиту, потім створюємо пари ід : [масив цін], якщо елемент з таким id існує то ми просто додаємо ще одну ціну , а якщо такого id ще немає у нашому об'єкті то створюється ключ з данним id
+  useEffect(() => {
+    if (product) {
+      product.forEach((item) => {
+        setPrices((prevPrices) => {
+          const newPrices = { ...prevPrices };
+          if (!newPrices[item.id]) {
+            newPrices[item.id] = [item.price];
+          } else {
+            newPrices[item.id].push(item.price);
+          }
+          return newPrices;
+        });
+      });
+    }
+  }, [product]);
+  // і або показуємо наш масив якщо ми вже знаємо id яке нам треба, або взагалі нічого не показуємо
+  if (prices[id]) {
+    return (
+      <ul
+        style={{
+          display: "flex",
+          justifyContent: "flex-start",
+          gap: "15px",
+          flexWrap: "wrap",
+        }}>
+        {prices[id].map((price, index) => (
+          <li key={index}>{price}</li>
+        ))}
+      </ul>
+    );
+  } else {
+    return null;
+  }
 }
